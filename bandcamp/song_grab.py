b57c8f10e6b7b6d5e83ce25d7b253b210f5d4f8e
Nicolas Gard <nicolasg699@gmail.com>
2019-03-10 03:17:57 -0500

    updated README, checking that repo is configured correctly

--- bandcamp/song_grab.py
+++ bandcamp/song_grab.py
@@ -1,27 +1,26 @@
 import os.path
-import bs4
+from bs4 import BeautifulSoup
+import requests
+import codecs
 
-from open_functions import open_page
-from open_functions import open_file
+def song_grab(url_to_grab, file_name, track_num):
+    page = requests.get(url_to_grab)
+    page_bs = BeautifulSoup(page.content, 'html.parser')
+    file = codecs.open(file_name, "a+", "utf-8")
 
-
-def song_grab(url, file_name, track_num):
-    page = open_page(url)
-    file = open_file(file_name)
-
-    song_name = page.find("h2", {"class": "trackTitle"}).text.strip()
-    abt_divfind = page.find("div", {"class": "tralbumData tralbum-about"})
+    song_name = page_bs.find("h2", {"class": "trackTitle"}).text.strip()
+    abt_divfind = page_bs.find("div", {"class": "tralbumData tralbum-about"})
     if abt_divfind is not None:
         abt_text = abt_divfind.text
         abt = ' '.join(abt_text.split())
     else:
         abt = ""
-    track_creds = page.find("div", {"class": "tralbumData tralbum-credits"})
+    track_creds = page_bs.find("div", {"class": "tralbumData tralbum-credits"})
     if track_creds is not None:
         creds_text = track_creds.text
         creds2 = ' '.join(creds_text.split())
         creds = creds2.split(",")[2]
-    song_duration = page.find("meta", {"itemprop": "duration"})['content']
+    song_duration = page_bs.find("meta", {"itemprop": "duration"})['content']
     write_string = str(track_num) + ", " + song_name + ", " + song_duration + ", " + abt + ", " + creds + "\n"
     # is it better to do one write all at once, maybe dont want 'abt' to add a new line for example
     file.write(write_string)
